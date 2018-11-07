a06c33e64bbc5f3895c736e8efd58b8448e9c709
Naomi Lui-HIng <naomiluihing@cmail.carleton.ca>
2018-11-07 13:20:01 -0500

    Creation of lightState()
    Creation of lightState(int colour) and test code for it within loop()

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,5 +1,8 @@
+#include <Ethernet.h>
 #include <HCSR04.h>
-//Ultrasonic inits
+#include <LiquidCrystal.h>
+
+//Ultrasonic initial settings
 //Initialize Ultrasonic Sensors and the pins it uses
 int trigPin1 = 10; //trigger pin connected to pin 11
 int echoPin1 = 11; //echo Pin connected to pin 12
@@ -21,45 +24,98 @@ bool car; //state variable if car is at parking lot entrance or not
 #define redPort A0
 #define greenPort A1
 #define bluePort A2
+//colour definitions to be passed to light state
+int red = 0;
+int green = 1;
+int blue = 2;
+int yellow = 3;
 
 
-void setup() 
+void setup()
 {
     //Initialize serial connection
     Serial.begin(9600);
 }
 
-void loop() 
+void loop()
 {
-    d1 = distanceSensor1.measureDistanceCm();
-    d2 = distanceSensor2.measureDistanceCm();
-    car = isCar(d1,d2); //test if car is there or not
+    lightState(red);
+    delay(wait);
+    lightState(green);
+    delay(wait);
+    lightState(blue);
+    delay(wait);
+    lightState(yellow);
+    delay(wait);
+//    d1 = distanceSensor1.measureDistanceCm();
+//    d2 = distanceSensor2.measureDistanceCm();
+//    car = isCar(d1, d2); //test if car is there or not
+//    
+//    Serial.print("D1: ");
+//    Serial.print(d1);
+//    Serial.println("cm");
+//    Serial.print("D2: ");
+//    Serial.print(d2);
+//    Serial.println("cm");
+//    if (car)
+//    {
+//        Serial.println("There is a car");
+//        }
+//    else
+//    {
+//        Serial.println("There is not a car.");
+//    }
+//
+//    delay(wait); //using predetermined time, in milliseconds, delay after each measurement and return
 
-    Serial.print("D1: ");
-    Serial.print(d1);
-    Serial.println("cm");
-    Serial.print("D2: ");
-    Serial.print(d2);
-    Serial.println("cm");
-    if (car)
-    {
-        Serial.println("There is a car");
-    }
-    else
-    {
-        Serial.println("There is not a car.");    
-    }
-    
-    delay(wait); //using predetermined time, in milliseconds, delay after each measurement and return
-    
 }
 
 
 bool isCar(double d1, double d2)
 {
-    if (d1 <=dTrig && d2 <= dTrig)
+  if (d1 <= dTrig && d2 <= dTrig)
+  {
+    return true;
+  }
+  return false;
+}
+
+
+void lightState(int colour)
+{
+    if (colour == 0) // Light to be set to red
     {
-        return true;
+        digitalWrite(redPort, HIGH);
+        digitalWrite(greenPort, LOW);
+        digitalWrite(bluePort, LOW);
+        Serial.println("red");
+    }   
+    else if (colour == 1)//Light to be set to Green
+    {
+        digitalWrite(redPort, LOW);
+        digitalWrite(greenPort, HIGH);
+        digitalWrite(bluePort, LOW);
+        Serial.println("green");
     }
-    return false;
+    else if (colour == 2)//Light to be set to blue
+    {
+        digitalWrite(redPort, LOW);
+        digitalWrite(greenPort, LOW);
+        digitalWrite(bluePort, HIGH);
+        Serial.println("blue");
+    }
+    else if (colour == 3)//Light to be set to yellow
+    {
+        digitalWrite(redPort, HIGH);
+        digitalWrite(greenPort, HIGH);
+        digitalWrite(bluePort, LOW);
+        Serial.println("yellow");
+    }
+    else //Light is not set
+    {
+        digitalWrite(redPort, HIGH);
+        digitalWrite(greenPort, LOW);
+        digitalWrite(bluePort, LOW);
+        Serial.println("not set");
+    }
 }
