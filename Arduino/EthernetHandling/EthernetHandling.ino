b5a98b694c2de29fc24cdd8b9b25fa640e59337d
thefrenchfry <josh.s.fryer@gmail.com>
2018-11-07 16:17:26 -0500

    Arduino-Server connection functional
    The Pi can now see and ping the arduino with this setup.

--- Arduino/EthernetHandling/EthernetHandling.ino
+++ Arduino/EthernetHandling/EthernetHandling.ino
@@ -4,14 +4,16 @@
 // Partially adapted from the WebClient sample program.
 
 // A6:8F:4E:6E:F5:B0; this is a valid but entirely arbitrary MAC address.
+// The shield does not come with a preset MAC so one needs to be set.
 byte mac[] = {0xA6, 0x8F, 0x4E, 0x6E, 0xF5, 0xB0};
 
-// 192.168.0.99; another arbitrary static IP
-// TODO: Work out our static IP assignments
-byte ip[] = {192, 168, 0, 99};
+// 10.0.0.43 should be this device's static IP
+//byte ip[] = {192, 168, 0, 99};
+IPAddress ip(10,0,0,43);
 
 // Define a server to connect to by IP address
-IPAddress server(169,254,45,60);
+// 10.0.0.41 should be the Server Pi
+IPAddress server(10,0,0,41);
 
 // Declare the client
 EthernetClient client;
@@ -20,7 +22,7 @@ void setup() {
   Serial.begin(9600);
   while(!Serial){}; // Wait until the serial port is able to connect.
 
-  // 
+  // Set up the Arduino with a static IP
   Serial.println("Attempting to initialize Ethernet with static IP");
   Ethernet.begin(mac, ip);
   Serial.print("IP address is ");
