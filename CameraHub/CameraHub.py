d01b0944a22862edc7ddec850b6144b5b08f02bf
thefrenchfry <josh.s.fryer@gmail.com>
2018-11-09 17:01:42 -0500

    Create function outlines

---
+++ CameraHub/CameraHub.py
@@ -0,0 +1,24 @@
+import requests
+import datetime
+import json
+
+# The IP address of the server is 10.0.0.41, port 80
+ipAddress = "http://10.0.0.41:80"
+
+
+def buildRequest(hubImages):
+    return
+
+def sendRequest(builtRequest):
+    return
+
+def captureImage(camID:int):
+    return
+
+
+
+class Image:
+    def __init__(self, photo, camID, time):
+        self.photo = photo
+        self.camID = camID
+        self.time = time
