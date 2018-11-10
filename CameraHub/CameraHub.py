81c1811e38ff568dacb93e34343c159ba5e6c5cc
thefrenchfry <josh.s.fryer@gmail.com>
2018-11-10 16:15:41 -0500

    Add part of captureImage and loadImage, and comments

--- CameraHub/CameraHub.py
+++ CameraHub/CameraHub.py
@@ -8,18 +8,29 @@ serverAddress = "http://10.0.0.41:80"
 # Defining endpoints
 imageEndpoint = serverAddress + "/Images"
 
+# Create an empty list to store images in
+hubImages = list()
 
+"""
+Compiles all of the Images currently held in hubImages into a JSON blob.
+Specifically, the format of the JSON is an array of objects, each object
+representing an Image.
+"""
 def buildRequest(hubImages):
     imgData = list()
+
     # Create a JSON object out of each image in hubImages
     for img in hubImages:
         # Expected order of JSON elements: camera ID, photo object, timestamp
-        obj = {"camID":img.camID, "photo":img.photo, "time":img.time}
+        obj = {"camID": img.camID, "photo": img.photo, "time": img.time}
         imgData.append(obj)
-    # builtRequest = json.dumps(imgData)
+
     return json.dumps(imgData)
 
 
+"""
+Send a JSON array containing Image objects to the proper endpoint of the server.
+"""
 def sendRequest(builtRequest):
     r = requests.post(imageEndpoint, builtRequest)
     print("Status code: " + r.status_code)
@@ -27,8 +38,21 @@ def sendRequest(builtRequest):
         print("Status code OK")
 
 
+"""
+Capture an image from the Pi Camera and transform it into an image object.
+"""
 def captureImage(camID:int):
-    return
+    # From what I've read, the picam saves images directly to a file? -- JF
+    path = "somepath"
+    return Image(loadImage(path), camID, datetime.datetime.now().strftime("%c"))
+
+
+"""
+Load a .JPG file as bytes.
+"""
+def loadImage(path):
+    img = open(path, 'r')
+    return img.read()
 
 
 class Image:
