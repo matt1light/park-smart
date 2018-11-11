31cffa9310420559a96fd360021451fc1aecd332
thefrenchfry <josh.s.fryer@gmail.com>
2018-11-10 20:54:36 -0500

    Defined displayState as struct, added dummy sendDisplayState
    The function doesn't do much useful, just creates and returns a dummy DisplayState.

--- Arduino/EthernetHandling/EthernetHandling.ino
+++ Arduino/EthernetHandling/EthernetHandling.ino
@@ -3,6 +3,10 @@
 
 // Partially adapted from the WebClient sample program.
 
+struct DisplayState{
+  int lightState[];
+  int screenState[];
+};
 // A6:8F:4E:6E:F5:B0; this is a valid but entirely arbitrary MAC address.
 // The shield does not come with a preset MAC so one needs to be set.
 byte mac[] = {0xA6, 0x8F, 0x4E, 0x6E, 0xF5, 0xB0};
@@ -18,8 +22,6 @@ IPAddress server(169,254,45,60);
 // Declare the client
 EthernetClient client;
 
-void setup() {
-  Serial.begin(9600);
 void startEthernet(void){
 Serial.begin(9600);
   while(!Serial){}; // Wait until the serial port is able to connect.
@@ -41,8 +43,18 @@ Serial.begin(9600);
   }
 }
 
-void loop() {
-  while(1){}; // Do nothing for now
+struct DisplayState sendDisplayState(int displaySystemID){
+  // Dummy function for now
+  // NB: I don't know if we've altered the details of displaystate?
+  // I'm going off the latest version of the class diagram.
+  //foobar
+  struct DisplayState currentDisplay;
+  int currLS[] = {1,2,3};
+  int currSS[] = {44,45};
+  // memcpy copies data from a source into a memory block
+  memcpy(currentDisplay.lightState, currLS, sizeof currLS);
+  memcpy(currentDisplay.screenState, currSS, sizeof currSS);
+  return currentDisplay;
 }
 
 void setup(){}
