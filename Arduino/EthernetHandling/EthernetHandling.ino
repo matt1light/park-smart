5f658d8f736db1a132423ccb598bfdaba005228a
thefrenchfry <josh.s.fryer@gmail.com>
2018-11-10 20:49:32 -0500

    Add stub sendDisplayState method
    It just returns a dummy DisplayState struct, nothing actually useful yet.

--- Arduino/EthernetHandling/EthernetHandling.ino
+++ Arduino/EthernetHandling/EthernetHandling.ino
@@ -43,8 +43,17 @@ Serial.begin(9600);
   }
 }
 
-void sendDisplayState(int displaySystemID){
-
+struct DisplayState sendDisplayState(int displaySystemID){
+  // Dummy function for now
+  // NB: I don't know if we've altered the details of displaystate?
+  // I'm going off the latest version of the class diagram.
+  struct DisplayState currentDisplay;
+  int currLS[] = {1,2,3};
+  int currSS[] = {44,45};
+  // memcpy copies data from a source into a memory block
+  memcpy(currentDisplay.lightState, currLS, sizeof currLS);
+  memcpy(currentDisplay.screenState, currSS, sizeof currSS);
+  return currentDisplay;
 }
 
 void setup(){}
