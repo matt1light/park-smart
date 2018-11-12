185f4a39d425cae77be0680f9a5c2db16ac80ed3
Naomi Lui-HIng <naomiluihing@cmail.carleton.ca>
2018-11-12 12:11:57 -0500

    Update Arduino/Arduino.ino
    Updated pin assignments, LEDs work, LCD works and aacar be sent stuff to display

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -2,12 +2,12 @@
 #include <HCSR04.h>
 #include <LiquidCrystal.h>
 
-//Ultrasonic initial settings
-//Initialize Ultrasonic Sensors and the pins it uses
-int trigPin1 = 6;//trigger pin for ultrasonic sensor 1 connected to pin 6
-int echoPin1 = 7;//echo Pin for ultrasonic sensor 1 connected to pin 7
-int trigPin2 = 8;//trigger pin for ultrasonic sensor 2 connected to pin 8
-int echoPin2 = 9;//echo Pin for ultrasonic sensor 2 connected to pin 9
+////Ultrasonic initial settings
+////Initialize Ultrasonic Sensors and the pins it uses
+#define trigPin1 A5//trigger pin for ultrasonic sensor 1 connected to pin 6
+#define echoPin1 A4//echo Pin for ultrasonic sensor 1 connected to pin 7
+#define trigPin2 8//trigger pin for ultrasonic sensor 2 connected to pin 8
+#define echoPin2 9//echo Pin for ultrasonic sensor 2 connected to pin 9
 
 double d1, d2;//distance values, one for each ultrasonic sensor
 
@@ -20,121 +20,161 @@ UltraSonicDistanceSensor distanceSensor2(trigPin2, echoPin2);
 
 bool car; //state variable if car is at parking lot entrance or not
 
-//LED setup
-#define redPort 2 //LED red pin set to port 2
-#define greenPort 3 //LED green pin set to port 3
-#define bluePort 4 //LED blue pin set to port 4
+////LED setup
+#define YELLOW1 A0
+#define GREEN1 A1
+#define YELLOW2 A2
+#define GREEN2 A3
 
 //colour definitions to be passed to light state
-int red = 0;
+int off = 0;
 int green = 1;
-int blue = 2;
-int yellow = 3;
+int yellow = 2;
+
+//maximum number of spots in the lot
+int maxSpots = 12;
+int numCars = 0;
+int freeSpots;
 
 //LCD Setup
-LiquidCrystal lcd(19,18,17,16,15,14); //Declaration of lcd using LiquidCrystal library setup
+const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup()
 {
-    //Initialize serial connection
-    Serial.begin(9600);
+  //Initialize serial connection
+  Serial.begin(9600);
 
-    //Initialize lcd interface and set dimentions
-    lcd.begin(16,2);
+  //Initialize lcd interface and set dimentions
+  lcd.begin(16, 2);
+  lcd.print("Free spots:");
 
-    setupEthernet();
+  pinMode(YELLOW1,OUTPUT);
+  pinMode(GREEN1, OUTPUT);
+  pinMode(YELLOW2,OUTPUT);
+  pinMode(GREEN2, OUTPUT);
+
 }
 
 void loop()
 {
 
-    //Test code for LED
-    lightState(red);
-    delay(wait);
-    lightState(green);
-    delay(wait);
-    lightState(blue);
-    delay(wait);
-    lightState(yellow);
-    delay(wait);
+  //test for lcd update
+  lcd.setCursor(0, 1);
+  numCars = 7;
+  updateLCD(numCars);
+    
+      //Test code for LED
+      delay(wait);
+      lightState(1,green);
+      lightState(2,green);
+      delay(wait);
+      lightState(1,yellow);
+      lightState(2,yellow);
+      delay(wait);
+      lightState(1,off);
+      lightState(2,off);
+      delay(wait);
 
-//    car = isCar(d1, d2); //test if car is there or not
-//   
-//Test code to see if car is present and display info for it 
-//    Serial.print("D1: ");
-//    Serial.print(d1);
-//    Serial.println("cm");
-//    Serial.print("D2: ");
-//    Serial.print(d2);
-//    Serial.println("cm");
-//    if (car)
-//    {
-//        Serial.println("There is a car");
-//        }
-//    else
-//    {
-//        Serial.println("There is not a car.");
-//    }
-//delay for the car test
-//    delay(wait); //using predetermined time, in milliseconds, delay after each measurement and return
+
+  //    car = isCar(d1, d2); //test if car is there or not
+  //    if (car)
+  //    {
+  //        numCars+=1;
+  //        updateLCD(numCars);
+  //    }
+  //Test code to see if car is present and display info for it
+  //    Serial.print("D1: ");
+  //    Serial.print(d1);
+  //    Serial.println("cm");
+  //    Serial.print("D2: ");
+  //    Serial.print(d2);
+  //    Serial.println("cm");
+  //    if (car)
+  //    {
+  //        Serial.println("There is a car");
+  //        }
+  //    else
+  //    {
+  //        Serial.println("There is not a car.");
+  //    }
+  //delay for the car test
+  //    delay(wait); //using predetermined time, in milliseconds, delay after each measurement and return
 
 }
 
 
 bool isCar()
 {
-    d1 = distanceSensor1.measureDistanceCm();
-    d2 = distanceSensor2.measureDistanceCm();
-    if (d1 <= dTrig && d2 <= dTrig)
-    {
-        return true;
-    }
-    return false;
+  d1 = distanceSensor1.measureDistanceCm();
+  d2 = distanceSensor2.measureDistanceCm();
+  if (d1 <= dTrig && d2 <= dTrig)
+  {
+    return true;
+  }
+  return false;
 }
 
 
 
-void lightState(int colour)
+void lightState(int row, int colour)
 {
-    if (colour == 0) // Light to be set to red
+  if (row == 1)
+  {
+    if (colour == green)//Light to be set to Green
     {
-        digitalWrite(redPort, HIGH);
-        digitalWrite(greenPort, LOW);
-        digitalWrite(bluePort, LOW);
-        Serial.println("red");
-    }   
-    else if (colour == 1)//Light to be set to Green
-    {
-        digitalWrite(redPort, LOW);
-        digitalWrite(greenPort, HIGH);
-        digitalWrite(bluePort, LOW);
-        Serial.println("green");
+      digitalWrite(GREEN1, HIGH);
+      digitalWrite(YELLOW1, LOW);
+      Serial.println("green");
     }
-    else if (colour == 2)//Light to be set to blue
+    else if (colour == yellow)//Light to be set to yellow
     {
-        digitalWrite(redPort, LOW);
-        digitalWrite(greenPort, LOW);
-        digitalWrite(bluePort, HIGH);
-        Serial.println("blue");
+      digitalWrite(GREEN1, LOW);
+      digitalWrite(YELLOW1, HIGH);
+      Serial.println("yellow");
     }
-    else if (colour == 3)//Light to be set to yellow
+    else if (colour == off) //Light is not set
     {
-        digitalWrite(redPort, HIGH);
-        digitalWrite(greenPort, HIGH);
-        digitalWrite(bluePort, LOW);
-        Serial.println("yellow");
+      digitalWrite(GREEN1, LOW);
+      digitalWrite(YELLOW1, LOW);
+      Serial.println("not set");
     }
-    else //Light is not set
+  }
+
+  else if (row == 2)
+  {
+    if (colour == green)//Light to be set to Green
     {
-        digitalWrite(redPort, HIGH);
-        digitalWrite(greenPort, LOW);
-        digitalWrite(bluePort, LOW);
-        Serial.println("not set");
+      digitalWrite(GREEN2, HIGH);
+      digitalWrite(YELLOW2, LOW);
+      Serial.println("green");
     }
+    else if (colour == yellow)//Light to be set to yellow
+    {
+      digitalWrite(GREEN2, LOW);
+      digitalWrite(YELLOW2, HIGH);
+      Serial.println("yellow");
+    }
+    else if(colour == off) //Light is not set
+    {
+      digitalWrite(GREEN2, LOW);
+      digitalWrite(YELLOW2, LOW);
+      Serial.println("not set");
+    }
+  }
+
 }
 
-
-void updateLCD()
+void updateLCD(int numCars)
 {
-    
+
+  if (numCars <= maxSpots)
+  {
+    freeSpots = maxSpots - numCars;
+    lcd.print(freeSpots);
+  }
+  else
+  {
+    lcd.print("No free spots");
+  }
 }
