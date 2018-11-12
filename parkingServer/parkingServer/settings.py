e7cceedbb6407201ea5e00118e2ae0e273b0a337
matt1light <matt1light@gmail.com>
2018-11-12 13:11:56 -0500

    fix improper imageProcessor configuration

--- parkingServer/parkingServer/settings.py
+++ parkingServer/parkingServer/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'inputAPI.apps.InputapiConfig',
     'outputAPI.apps.OutputapiConfig',
-    'imageProcessor.apps.ImageProcessorConfig',
+    'imageProcessor.apps.ImageprocessorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
