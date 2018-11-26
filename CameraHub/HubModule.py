75a6822430a16d8dc369283407ec32e703dc7589
Dare Balogun <mroludarebalogun@yahoo.ca>
2018-11-26 13:13:52 -0500

    Change request to single image at a time

--- CameraHub/HubModule.py
+++ CameraHub/HubModule.py
@@ -1,16 +1,25 @@
 from CameraHub import CameraHub
+import time
 
-hub1 = CameraHub() 
-    #Capture an Image for all cameras in the hub
-for camera in hub1.hubCameras:
-    x = hub1.hubCameras.index(camera)
-    hub1.captureImage(x)
-     
-    #Build a request after all images have been captured 
-hub1.buildRequest()
+#delay between Image captures
+delay = 60
+
+while (1):
+    # Create a Camera Hub object and initialize it
+    hub1 = CameraHub() 
     
+    # Capture an Image for all cameras in the hub
+    for camera in hub1.hubCameras:
+        x = hub1.hubCameras.index(camera)
+        hub1.captureImage(x)
+         
+    #Build a request after all images have been captured 
+    hub1.buildRequest()
+        
     #Send request
-print(hub1.builtRequest.data)
+    hub1.sendRequest()
+
+    #print(hub1.responseCode)
 
     #Delay
-#time.sleep(delay)
+    time.sleep(delay)
