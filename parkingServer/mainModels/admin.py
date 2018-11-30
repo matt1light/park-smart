2c08203e2a0827b39b78f7b93aca24a029933d9b
matt1light <matt1light@gmail.com>
2018-11-30 18:04:47 -0500

    add all models to admin dashboard

--- parkingServer/mainModels/admin.py
+++ parkingServer/mainModels/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from django.apps import apps
 
-from .models import Image
+app = apps.get_app_config("mainModels")
 
 # Register your models here.
-admin.site.register(Image)
+for model_name, model in app.models.items():
+    admin.site.register(model)
+
+
