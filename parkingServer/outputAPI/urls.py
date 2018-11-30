9946d7c985dc71b77f7a46918e9e359772d8c7f5
matt1light <matt1light@gmail.com>
2018-11-30 17:58:52 -0500

    add admin panel

--- parkingServer/outputAPI/urls.py
+++ parkingServer/outputAPI/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 # from .views import DetailsView
 from .views import get_display_state
 
-urlpatterns = {
+urlpatterns = [
     url(r'^displayState/$', get_display_state, name="details"),
-}
+]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
