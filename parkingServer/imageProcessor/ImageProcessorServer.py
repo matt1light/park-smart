fc3152fe13095d0394527de665226b27b7f788cc
matt1light <matt1light@gmail.com>
2018-12-01 18:23:26 -0500

    Image Processing tweaks

--- parkingServer/imageProcessor/ImageProcessorServer.py
+++ parkingServer/imageProcessor/ImageProcessorServer.py
@@ -1,5 +1,5 @@
 from abc import abstractmethod
-from .ImageAI import getCoordsFromImageResnet
+from .ImageAI import getCoordsFromImageResnet, getCarAndMotorcycleCoordsFromImageResnet
 from .visionAPI import localize_objects
 
 class ImageProcessorServer():
@@ -7,11 +7,17 @@ class ImageProcessorServer():
     @abstractmethod
     def get_car_coordinates(image_path):
         print("implement this method")
+    @abstractmethod
+    def get_car_coordinates_calibrate(image_path):
+        print("implement this method")
 
 class ImageProcessorServerVisionAPI(ImageProcessorServer):
 
     @staticmethod
     def get_car_coordinates(image_path):
+        return localize_objects(image_path)
+    @staticmethod
+    def get_car_coordinates_calibrate(image_path):
         return localize_objects(image_path)
 
 class ImageProcessorServerImageAI(ImageProcessorServer):
@@ -19,3 +25,7 @@ class ImageProcessorServerImageAI(ImageProcessorServer):
     @staticmethod
     def get_car_coordinates(image_path):
         return getCoordsFromImageResnet(image_path)
+    @staticmethod
+    def get_car_coordinates_calibrate(image_path):
+        return getCarAndMotorcycleCoordsFromImageResnet(image_path)
+
